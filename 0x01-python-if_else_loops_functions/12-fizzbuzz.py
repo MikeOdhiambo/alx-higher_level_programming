41d0018018604c6c39d9fc795f4930145144096d
MikeOdhiambo <odhiambomike413@gmail.com>
2022-05-31 06:40:16 -0700

    Solution 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", sep=" ")
+        elif i % 5 == 0:
+            print("Buzz", sep=" ")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", sep=" ")
+        else:
+            print(f"{i}", sep=" ")
