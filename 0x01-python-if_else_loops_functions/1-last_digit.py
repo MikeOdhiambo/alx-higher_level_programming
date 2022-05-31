124ea3d82017d80e16b6ac3dbc1fd7f4ddd075ea
MikeOdhiambo <odhiambomike413@gmail.com>
2022-05-31 12:37:10 -0700

    use string format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number < 0:
 else:
     last_dig = number % 10
 if last_dig == 0:
-    print("Last digit of {} is {} and is 0".format(number, last))
+    print("Last digit of {} is {} and is 0".format(number, last_dig))
 elif last_dig > 0 and last_dig < 6:
-    print("Last digit of {} is {} and is 0".format(number, last))
+    print("Last digit of {} is {} and is 0".format(number, last_dig))
 elif last_dig > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last))
+    print("Last digit of {} is {} and is greater than 5".format(number, last_dig))
