4b818c5f1dcefb12ace9ecd53ee19234bad77467
MikeOdhiambo <odhiambomike413@gmail.com>
2022-05-31 11:45:06 -0700

    Add else statement

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,7 @@
 def print_last_digit(number):
     if number < 0:
         last = (number * -1) % 10
-    last = number % 10
+    else:
+        last = number % 10
+    print(f"{last}", end="")
     return last
