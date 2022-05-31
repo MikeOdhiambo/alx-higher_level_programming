9beac0ea51e3d4b9521cd5d87ab26e85559e06f0
MikeOdhiambo <odhiambomike413@gmail.com>
2022-05-31 12:30:09 -0700

    use string format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_dig = (number * -1) % 10
+    last_dig = ((number * -1) % 10) * -1
 else:
     last_dig = number % 10
-
 if last_dig == 0:
-    print(f"Last digit of {number:d} is {last_dig:d} and is 0")
+    print("Last digit of {} is {} and is 0".format(number, last))
 elif last_dig > 0 and last_dig < 6:
     print(
-        f"Last digit of {number:d} is "
-        f"{last_dig:d} and is less than 6 and not 0"
+        "Last digit of {} is {} and is\ 
+        less than 6 and not 0".format(number, last))
         )
 elif last_dig > 5:
-    print(f"Last digit of {number:d} is {last_dig:d} and is greater than 5")
+    print("Last digit of {} is {} and is greater than 5".format(number, last))
