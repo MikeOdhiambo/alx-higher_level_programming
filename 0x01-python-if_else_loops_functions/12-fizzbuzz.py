e426886f63be83f2704ffa08f2ba89c726679d04
MikeOdhiambo <odhiambomike413@gmail.com>
2022-05-31 12:40:01 -0700

    use elif statements

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,12 +4,9 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz", end=" ")
-            continue
-        if i % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz", end=" ")
-            continue
-        if i % 15 == 0:
+        elif i % 15 == 0:
             print("FizzBuzz", end=" ")
-            continue
         else:
-            print(f"{i}", end=" ")
+            print(i, end=" ")
