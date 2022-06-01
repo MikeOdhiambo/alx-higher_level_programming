72d68fa15f2b822f948f6cf3728bbb38e95e77d5
MikeOdhiambo <odhiambomike413@gmail.com>
2022-05-31 19:38:39 -0700

    Whitespace error corrections

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_dig = ((number * -1) % 10) * -1
+    lst = ((number * -1) % 10) * -1
 else:
-    last_dig = number % 10
-if last_dig == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_dig))
-elif last_dig != 0 and last_dig < 6:
-    print("Last digit of {} is {} and is less",
-            " than 6 and not 0".format(number, last_dig))
-elif last_dig > 5:
-    print("Last digit of {} is {} and is greater",
-            " than 5".format(number, last_dig))
+    lst = number % 10
+if lst == 0:
+    print("Last digit of {} is {} and is 0".format(number, lst))
+elif lst != 0 and lst < 6:
+    print("Last digit of {0} is {1} and is less\
+             than 6 and not 0").format(number, lst)
+elif lst > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, lst))
