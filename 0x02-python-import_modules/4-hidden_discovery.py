9fc8c800812be81bed28610fb12b22eceb3c0ebc
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-02 02:51:12 -0700

    Revert "Correct indexing"
    This reverts commit 82dd29c8e00ebd86a1a17b8888653695c1397d79.

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4    
     for name in dir(hidden_4):
-        if name[1] == '_':
+        if name[0] == '_':
             continue
         else:
             print(name)
