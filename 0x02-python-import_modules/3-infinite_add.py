3db24d893ee0e553630980175a60ddee32abc68b
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-02 02:43:14 -0700

    Solution 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    
+    sum = 0
+    if(len(argv) < 2):
+        print("{}".format(sum))
+    else:
+        for idx in range(1, len(argv)):
+            sum += int(argv[idx])
+        print("{}".format(sum))
