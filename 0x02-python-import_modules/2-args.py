054af90312f44c1b76fcd8b675b8aae6f1077eae
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-02 02:58:20 -0700

    Correct whitespace errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    
     argc = len(argv)
     if argc == 1:
         print("0 arguments.")
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    
     sum = 0
     if(len(argv) < 2):
         print("{}".format(sum))
