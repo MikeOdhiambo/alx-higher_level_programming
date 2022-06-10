6fbe55861a2155583167c0e10fd8fc9da6cb0048
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-10 10:48:52 -0700

    Refactor function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    func = lambda x: x if (x != search) else replace
-    rep_list = list(map(func, my_list))
+    rep_list = list(map(lambda x: x if (x != search) else replace, my_list))
     return rep_list
