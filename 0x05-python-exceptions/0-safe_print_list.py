97917defe7b1bb829bdab063966ad5a8717c0f83
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-20 14:09:31 -0700

    Refactor counters

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    list_len = 1
-    for list_len in range((x + 1)):
+    list_len = 0
+    for list_len in range(x):
         try:
             print("{}".format(my_list[list_len]), end="")
         except IndexError:
             break
+        else:
+            list_len += 1
 
     print("")
     return list_len
