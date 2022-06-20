a13da20c1c555d0ec12b4989112e83f0141e10ab
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-20 14:54:42 -0700

    MOve return statement

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,7 +7,7 @@ def safe_function(fct, *args):
     res = 0
     try:
         res = fct(a, b)
+        return res
     except Exception as err:
         sys.stderr.write("Exception: {}\n".format(err))
         res = None
-    return res
