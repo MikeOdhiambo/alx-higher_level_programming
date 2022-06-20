be68bc929721b757c1a764736cf93f7ded1fec60
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-20 15:09:22 -0700

    Redo

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-        except IndexError:
-            i = i - 1
-            break
-    print("")
     try:
-        return i + 1
-    except UnboundLocalError:
-        return 0
+        for idx in range(x):
+            print(my_list[idx], end="")
+        return x
+    except IndexError:
+        return idx
+    finally:
+        print()
