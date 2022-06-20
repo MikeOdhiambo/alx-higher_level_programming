1d678a51d74620729a415a18adcf65dbcf782967
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-20 14:04:07 -0700

    Add string formater

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     list_len = 1
     for list_len in range((x + 1)):
         try:
-            print(my_list[list_len], end="")
+            print("{}".format(my_list[list_len]), end="")
         except IndexError:
             break
 
