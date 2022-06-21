b0ecd8373605f861598fad167bdf7f10ec992491
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-21 11:46:07 -0700

    Solution 0 and 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A Square class definition
+
+    Attributes:
+        size(int): Length of side of square
+    """
+
+    def __init__(self, size):
+        """
+        Initialize the Square class
+
+        Args:
+            size (int): Length of side of square
+        """
+        self.__size = size
