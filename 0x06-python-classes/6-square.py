b0c5f3c8e636c2b146822e88c5a1bbca9236fe23
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-21 13:39:32 -0700

    Refactor value check

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,9 +43,9 @@ class Square:
     @position.setter
     def position(self, value):
         """ Set the coordinate to value """
-        if not isinstance(value, tuple) or len(value) != 2:
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
