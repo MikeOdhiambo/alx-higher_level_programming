cd946ae80cd1ca8fcc50b61fc6e5bc8ccd55b49c
MikeOdhiambo <odhiambomike413@gmail.com>
2022-06-27 17:20:24 -0700

    Change comparison operators

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
         Args:
             value(int): New width
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -48,7 +48,7 @@ class Rectangle:
         Args:
             value(int): New height
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
