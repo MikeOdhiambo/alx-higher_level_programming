1499e412427eae8d01eb8891b0b0b175a086bd97
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-10 02:20:24 +0300

    Solution 2 and 3

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes the database hbtn_0c_0
+-- Should not fail if database doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- Shows tables of a database with name passed as argument
+SHOW TABLES
