50e90afd1a88ea85a319bdf231edccf6f33d66a0
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-10 03:46:16 +0300

    Fix count param

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- Lists the number of records with the same score in the table second_table
 -- Result displays the score and number of records for this score with label 'number'
 --- List is sorted by number of records (DESC)
-SELECT score, COUNT(*) AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
