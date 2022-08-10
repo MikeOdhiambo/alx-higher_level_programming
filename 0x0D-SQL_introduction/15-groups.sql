7114eda4148968d86118da78c7aeb36a15b62b6c
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-10 04:07:09 +0300

    Make as uppercase

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Lists the number of records with the same score in the table second_table
 -- Result displays the score and number of records for this score with label 'number'
 --- List is sorted by number of records (DESC)
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
