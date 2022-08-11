410d549605150791e5127153b988dc271ce5dfba
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-11 04:24:53 +0300

    Fix password setting

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates the MySQL user user_0d_1 with all privileges if they don't already exist
 -- Sets the password to user_0d_1_pwd
-CREATE USER IF NOT ALREADY EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT ALREADY EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
