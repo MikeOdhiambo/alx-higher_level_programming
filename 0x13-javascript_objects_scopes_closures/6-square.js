91014d4d058e1678a1834dcd1d1b1c51b17fcd04
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-08 02:18:13 +0300

    Solution 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const _Square = require('./5-square');
+
+module.exports = class Square extends _Square {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
