388b9fb355ee23afc6eb53a17ffd9696e7f37793
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-10 02:48:35 +0300

    Add BINARY to ensure case sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT id, name
                 FROM states
-                WHERE name LIKE 'N%'
+                WHERE name LIKE BINARY 'N%'
                 ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
