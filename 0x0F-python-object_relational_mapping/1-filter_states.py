351739c6e81ea44f65e878cbb81c096928e1c405
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-10 02:58:55 +0300

    Triple quotations for multiline strings

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name
+    cur.execute("""SELECT id, name
                 FROM states
                 WHERE states.name LIKE BINARY 'N%'
-                ORDER BY states.id ASC")
+                ORDER BY states.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
