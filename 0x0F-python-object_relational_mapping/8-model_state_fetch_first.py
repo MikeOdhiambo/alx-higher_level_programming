70ddbfa614556e37631ec68b2565a5fc26396aeb
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-12 02:03:05 +0300

    Solutions 7-9

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Prints the first state object from database hbtn_0e_6_usa"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        f_state = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(f_state.id, f_state.name))
+    except:
+        print("Nothing")
---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Prints the first state object from database hbtn_0e_6_usa with letter a"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).\
+            filter(State.name.like('%a%')).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
