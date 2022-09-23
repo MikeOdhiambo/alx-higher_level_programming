ffb014bb2a4e764940e76040cc957dcd3ac0a5e0
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-24 02:54:49 +0300

    Solution 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sends request to URL and displays value of X-Request-Id"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    url = argv[1]
+    req = requests.get(url)
+    print(req.headers['X-Request-Id'])
