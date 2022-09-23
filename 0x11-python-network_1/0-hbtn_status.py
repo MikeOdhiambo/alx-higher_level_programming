84c755c5de82612ac18e89bc6424406f2c20786f
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-24 01:48:00 +0300

    Rename http content variable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     from urllib import request
 
     with request.urlopen('https://alx-intranet.hbtn.io/status') as req:
-        req = response.read()
+        cont = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(req)))
         print("\t- content: {}".format(req))
-        print("\t- utf8 content: {}".format(req.decode("utf-8")))
+        print("\t- utf8 content: {}".format(cont.decode("utf-8")))
