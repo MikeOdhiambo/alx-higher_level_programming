b74c7d2c4022fb9e4969467b726ade6d04c33663
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-23 03:07:57 +0300

    Use F option to specify key-value pairs

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a post request to URL with given headers
-curl -sIX "POST" -H "email: test@gmail.com" -H "subject: I will always be here for PLD"
+curl -sX "POST" -F "email=test@gmail.com" -F "subject=I will always be here for PLD"
