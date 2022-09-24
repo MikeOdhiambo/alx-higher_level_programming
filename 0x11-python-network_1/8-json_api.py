9995668d9bef9edf39cff279d22b40a1d1818f56
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-24 03:49:04 +0300

    Solution 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Takes in a letter, sends a POST request and displays search results"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = 'http://0.0.0.0:5000/search_user'
+    if argv[1]:
+        q = argv[1]
+    else:
+        q = ""
+
+    req = requests.post(url, data={'q': q})
+    try:
+        dct = req.json()
+        if dct == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(dct.get('id'), dct.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
