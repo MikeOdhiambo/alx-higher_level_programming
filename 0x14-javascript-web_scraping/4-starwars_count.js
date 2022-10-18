631033a76c0d377b7387ed113cea1573116f45b3
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-18 17:09:06 +0300

    Fix semistandard issues

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get(`https://swapi-api.hbtn.io/api/films/`, function (error, response, body) {
+request.get('https://swapi-api.hbtn.io/api/films/', function (error, response, body) {
   if (error) {
     console.log(error);
-  };
-  const wedge = "https://swapi-api.hbtn.io/api/people/18/";
+  }
+  const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
   const res = JSON.parse(body);
-  let count = 0
-  const resArray = res["results"];
-  //console.log(resArray[0]);
+  let count = 0;
+  const resArray = res.results;
+  // console.log(resArray[0]);
   for (let i = 0; i < resArray.length; i++) {
-	  //console.log(resArray[i]["characters"]);
-	  if ((resArray[i]["characters"].includes(wedge))) {
-		  count += 1
-	  }
-  };
+    if ((resArray[i].characters.includes(wedge))) {
+      count += 1;
+    }
+  }
   console.log(count);
 });
