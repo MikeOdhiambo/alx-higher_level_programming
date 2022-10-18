905c744a876f9b4323abbda84ed318b14f7c04fb
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-18 15:38:55 +0300

    Remove Check for response error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,5 @@
 
 const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
-  console.error(error);
   console.log('code:', response && response.statusCode);
 });
