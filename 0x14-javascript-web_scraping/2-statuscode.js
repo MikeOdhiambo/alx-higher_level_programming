d328839b312377f0854d94c840a1167fb05f486e
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-18 15:40:41 +0300

    Handle errors

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,8 @@
 
 const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
   console.log('code:', response && response.statusCode);
 });
