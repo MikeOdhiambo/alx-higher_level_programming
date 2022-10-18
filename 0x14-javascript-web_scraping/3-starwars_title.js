7371a3050a95a8fb19d36d3bbe8a219fa37c0f92
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-18 16:05:38 +0300

    Solution 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function(error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const res = JSON.parse(body);
+  console.log(res["title"]);
+});
