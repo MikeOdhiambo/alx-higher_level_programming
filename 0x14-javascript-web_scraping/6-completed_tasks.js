d6c1964d21ee95dbe4337fc2b30c252c862bcffb
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-22 03:41:26 +0300

    Solution 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const request = require('request');
+request.get(process.argv[2], function (err, res, body) {
+  const tasks = {};
+  const total = JSON.parse(body);
+  if (res.statusCode === 200) {
+    for (let i = 0; i < total.length; i++) {
+      if (total[i].completed) {
+        if ((total[i].userId in tasks)) {
+          tasks[total[i].userId] += 1;
+        } else {
+          tasks[total[i].userId] = 1;
+        }
+      }
+    }
+    console.log(tasks);
+  } else if (err) {
+    console.log(err);
+  }
+});
